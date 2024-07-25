9261bd7c5fa64e5e794964bff7857150ad2803a4
Vinay Chauhan <vinaychauhan.java@gmail.com>
2024-07-25 17:17:48 -0300

    Initial Commit for Java 17 Features

--- src/com/vinay/prg1/recordClasses/VinayPrg1.java
+++ src/com/vinay/prg1/recordClasses/VinayPrg1.java
@@ -1,5 +1,18 @@
 package com.vinay.prg1.recordClasses;
 
+/**
+ * Records classes are immutable and used as data transfer objects
+ * with zero boilerplate code. Record classes are defined with the
+ * record keyword. It always extends the java.lang.Record class.
+ * Example :- record Point(int x, int y) {}
+ * The above line of code generates functionality automatically like
+ * - All fields are private and final.
+ * - A canonical constructor for all fields.
+ * - Public accessor for all fields.
+ * - equals, hashcode, and toString methods.
+ * - Also, it can be defined locally either inside a class or method
+ */
+
 public class VinayPrg1 {
 
     public static void main(String[] args) {
